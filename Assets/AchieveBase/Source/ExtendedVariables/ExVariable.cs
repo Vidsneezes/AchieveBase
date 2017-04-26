9f02f526fe3e594a1b67810ece917862c87da277
Vidsneezes <moraleszez@gmail.com>
2017-04-25 19:45:59 -0600

    added custom type ExVariable

---
+++ Assets/AchieveBase/Source/ExtendedVariables/ExVariable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class ExVariable<T>{
+
+    private T _value;
+    public T value
+    {
+        get
+        {
+            return value;
+        }
+
+        set
+        {
+            _value = value; 
+            if(onValueChange != null)
+            {
+                onValueChange();
+            }
+        }
+    }
+
+    public Action onValueChange;
+
+}

0930445502696a430dc2cab2a37fac1dd7a4423c
Vidsneezes <moraleszez@gmail.com>
2017-04-26 16:44:20 -0600

    Added the AchieveBase class

---
+++ Assets/AchieveBase/Source/AchieveBase.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public static class AchieveBase  {
+
+    private static bool init = false;
+    private static VariableDatabase variableDatabase;
+
+    public static void Init()
+    {
+        init = true;
+        variableDatabase = new VariableDatabase();
+    }
+
+    public static bool GetVariable(string variableName, out BaseVariable variable)
+    {
+        if(!init)
+        {
+            Init();
+        }
+
+        if(variableDatabase.GetVariable(variableName,out variable))
+        {
+            return true;
+        }
+        return false;
+    }
+
+}

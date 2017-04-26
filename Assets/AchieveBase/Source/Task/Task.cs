04095ab531101cbf0c08deb87a5b12080179433b
Vidsneezes <moraleszez@gmail.com>
2017-04-25 19:53:44 -0600

    added Task class and conditional structs

---
+++ Assets/AchieveBase/Source/Task/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Task {
+    public string Title;
+    public string description;
+}
+
+public struct TaskConditionFloat
+{
+    public string valueToCheck;
+    public Comparors compareType;
+    public float expectedValue;
+}
+
+public struct TaskConditionBool
+{
+    public string valueToCheck;
+    public Comparors compareType;
+    public bool expectedValue;
+}
+
+public struct TaskConditionInt
+{
+    public string valueToCheck;
+    public Comparors compareType;
+    public int expectedValue;
+}
+
+public enum Comparors
+{
+    equal,
+    greaterThan,
+    lessThan,
+    greaterOrEqualThan,
+    lessOrEqualThan
+}

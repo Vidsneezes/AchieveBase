7e53d38b5a4fd34232961ef673b1f45cd8ad5dec
Vidsneezes <moraleszez@gmail.com>
2017-04-29 19:26:18 -0600

    added achievement editor base

---
+++ Assets/AchieveBase/Editor/AchievementEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AchievementEditor : EditorWindow {
+
+    [MenuItem("AchieveBase/Achievement Editor")]
+    public static void Init()
+    {
+        AchievementEditor ae = (AchievementEditor)EditorWindow.GetWindow(typeof(AchievementEditor));
+        ae.Show();
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

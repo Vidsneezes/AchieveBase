735edc115d8bd5929118de9827bd7d5d6ecaeaad
Vidsneezes <moraleszez@gmail.com>
2017-05-01 20:03:26 -0600

    added Data container

---
+++ Assets/AchieveBase/Source/DataContainer/VariableDataContainer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VariableDataContainer : ScriptableObject {
+
+    public List<ExFloat> floats;
+    public List<ExBool> bools;
+    public List<ExInt> ints;
+
+}
